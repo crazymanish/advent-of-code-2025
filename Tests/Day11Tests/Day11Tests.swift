dfaf6b15ba7eff64ddd9b3efab23b696d17f70c5
Manish Rathi <i.am.manish.rathi@gmail.com>
2025-12-11 06:16:23 +0100

    Day 11: Reactor - Path counting in directed graph (#13)
    Implement efficient path counting algorithm for device network using memoized DFS.
    Part 1 counts all paths from 'you' to 'out'.
    Part 2 counts paths visiting both 'dac' and 'fft' using path decomposition.
    
    - Use memoization to achieve O(V+E) complexity instead of exponential
    - Apply multiplication principle for conditional path counting
    - Add comprehensive documentation and inline comments

--- Tests/Day11Tests/Day11Tests.swift
+++ Tests/Day11Tests/Day11Tests.swift
@@ -3,15 +3,41 @@ import Testing
 
 struct Day11Tests {
     private let exampleInput = """
+    aaa: you hhh
+    you: bbb ccc
+    bbb: ddd eee
+    ccc: ddd eee fff
+    ddd: ggg
+    eee: out
+    fff: out
+    ggg: out
+    hhh: ccc fff iii
+    iii: out
+    """
+
+    private let exampleInput2 = """
+    svr: aaa bbb
+    aaa: fft
+    fft: ccc
+    bbb: tty
+    tty: ccc
+    ccc: ddd eee
+    ddd: hub
+    hub: fff
+    eee: dac
+    dac: fff
+    fff: ggg hhh
+    ggg: out
+    hhh: out
     """
 
     @Test func testPart1Example() {
         let day = Day11(input: exampleInput)
-        // #expect(day.part1() == expectedValue)
+        #expect(day.part1() == 5)
     }
 
     @Test func testPart2Example() {
-        let day = Day11(input: exampleInput)
-        // #expect(day.part2() == expectedValue)
+        let day = Day11(input: exampleInput2)
+        #expect(day.part2() == 2)
     }
 }
