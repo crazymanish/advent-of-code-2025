94334acd728607b40b9662a6226e5a9d2b3c0a77
Manish Rathi <i.am.manish.rathi@gmail.com>
2025-12-12 06:22:56 +0100

    Day 12: Christmas Tree Farm (#14)
    - Implement backtracking algorithm for present placement
    - Add lexicographic ordering optimization for identical shapes
    - Part 1: Count regions that can fit all presents (Answer: 492)
    - Part 2: Narrative conclusion (both stars earned)
    - Add comprehensive comments and improve variable names

--- Tests/Day12Tests/Day12Tests.swift
+++ Tests/Day12Tests/Day12Tests.swift
@@ -3,15 +3,43 @@ import Testing
 
 struct Day12Tests {
     private let exampleInput = """
+    0:
+    ###
+    ##.
+    ##.
+
+    1:
+    ###
+    ##.
+    .##
+
+    2:
+    .##
+    ###
+    ##.
+
+    3:
+    ##.
+    ###
+    ##.
+
+    4:
+    ###
+    #..
+    ###
+
+    5:
+    ###
+    .#.
+    ###
+
+    4x4: 0 0 0 0 2 0
+    12x5: 1 0 1 0 2 2
+    12x5: 1 0 1 0 3 2
     """
 
     @Test func testPart1Example() {
         let day = Day12(input: exampleInput)
-        // #expect(day.part1() == expectedValue)
-    }
-
-    @Test func testPart2Example() {
-        let day = Day12(input: exampleInput)
-        // #expect(day.part2() == expectedValue)
+        #expect(day.part1() == 2)
     }
 }
